072bfe34aca8e4d9c2d92ba5e2a2f96b5148ed9b
DESKTOP-M3D7JQ1\Roaa <rorokoraiem@gmail.com>
2023-04-27 20:15:54 +0200

    random color generating edits

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:splashscreen/splashscreen.dart';
 
 import 'home.dart';
 
@@ -15,28 +13,30 @@ class TestApplication extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Test App',
-      home: Splash(),
+      home: Home(),
     );
   }
 }
 
-class Splash extends StatelessWidget {
-  const Splash({super.key});
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  static const Widget appName = Text(
+    'Test Application',
+    style: TextStyle(
+      color: Colors.black,
+      fontSize: 48,
+      fontWeight: FontWeight.bold,
+    ),
+  );
 
   @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
   Widget build(BuildContext context) {
-    return SplashScreen(
-      seconds: 4,
-      navigateAfterSeconds: const Home(),
-      backgroundColor: const Color.fromARGB(255, 66, 245, 221),
-      title: const Text(
-        'Test Application',
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    );
+    return const Scaffold();
   }
 }
